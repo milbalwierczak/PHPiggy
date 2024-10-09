f4502aa30ad63d7a013b01ab51e4e7c1a4a505e8
Milosz Balwierczak <milosz.balwierczak.programista@gmail.com>
2024-10-09 19:33:13 +0200

    Addded containers and dependency injection

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -7,8 +7,10 @@ namespace Framework;
 class Router
 {
     private array $routes = [];
+    private array $middlewares = [];
 
-    public function add(string $method, string $path, $controller){
+    public function add(string $method, string $path, $controller)
+    {
         $path = $this->normalizePath($path);
 
         $this->routes[] = [
@@ -27,22 +29,42 @@ class Router
         return $path;
     }
 
-    public function dispatch(string $path, string $method)
+    public function dispatch(string $path, string $method, Container $container = null)
     {
         $path = $this->normalizePath($path);
         $method = strtoupper($method);
 
-        foreach($this->routes as $route) {
-            if (!preg_match("#^{$route['path']}$#", $path) ||
-            $route['method'] !== $method) {
+        foreach ($this->routes as $route) {
+            if (
+                !preg_match("#^{$route['path']}$#", $path) ||
+                $route['method'] !== $method
+            ) {
                 continue;
             }
 
             [$class, $function] = $route['controller'];
 
-            $controllerInstance = new $class;
+            $controllerInstance = $container ?
+                $container->resolve($class) :
+                new $class;
 
-            $controllerInstance->{$function}();
+            $action = fn() => $controllerInstance->{$function}();
+
+            foreach ($this->middlewares as $middleware) {
+                $middlewareInstance = $container ?
+                    $container->resolve($middleware) :
+                    new $middleware;
+                $action = fn() => $middlewareInstance->process($action);
+            }
+
+            $action();
+
+            return;
         }
+    }
+
+    public function addMiddleware(string $middleware)
+    {
+        $this->middlewares[] = $middleware;
     }
 }
