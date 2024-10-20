7c1d1f8578d9cc0d114fd61b51bb2afe13d39dfd
Milosz Balwierczak <milosz.balwierczak.programista@gmail.com>
2024-10-20 18:33:32 +0200

    Added form validation

---
+++ src/App/Middleware/SessionMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Framework\Contracts\MiddlewareInterface;
+use App\Exceptions\SesisonException;
+
+class SessionMiddleware implements MiddlewareInterface
+{
+    public function process(callable $next)
+    {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SesisonException("Session already active.");
+        }
+
+        if (headers_sent($filename, $line)) {
+            throw new SesisonException("Headers already sent. Consider enabling output buffering. Data outputted from {$filename} - Line: {$line}");
+        }
+
+        session_start();
+
+        $next();
+
+        session_write_close();
+    }
+}
